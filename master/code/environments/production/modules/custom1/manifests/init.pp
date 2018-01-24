0e8178838469c7e5e1d36c2eed615049fb0e10e0
Morgan Simonsen <morgan@simonsen.bz>
2018-01-24 21:31:11 +0000

    case for kerne

--- master/code/environments/production/modules/custom1/manifests/init.pp
+++ master/code/environments/production/modules/custom1/manifests/init.pp
@@ -4,17 +4,25 @@
 class custom1 (
   $root_password =  'SecretValue'
 ) {
-  # resources
   notify {"hello world ${root_password}":}
 
-  file { 'c:\users\kermit\secretfile.txt':
-    ensure  => 'present',
-    content => "${root_password}",
-  }
+  case $::kernel {
+    'Linux': {
+      file { '/tmp/secretfile.txt':
+      ensure  => 'present',
+      content => "${root_password}",
+      }
+    }
 
-  file { '/tmp/secretfile.txt':
-    ensure  => 'present',
-    content => "${root_password}",
+    'windows': {
+      file { 'c:\users\kermit\secretfile.txt':
+      ensure  => 'present',
+      content => "${root_password}",
+      }
+    }
+    default: {
+      # ...
+    }
   }
 }
 
